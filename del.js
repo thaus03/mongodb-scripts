94364a6b22cc26de9933cf5096cfd88afc64b4fb
Igor Carvalho <14116019+thaus03@users.noreply.github.com>
2021-05-10 12:18:58 -0300

    Update del.js
    Feitas algumas alterações na estrutura:
    
    * Removi o print dos ids
    * Como a execução da função pode se tornar demorada, coloquei para printar na tela as data de início e fim da execução e o total de registros removidos

--- del.js
+++ del.js
@@ -11,11 +11,12 @@ del("2020-01-01","2020-07-31")
 */
 
 function del(d1,d2) {
- 
- var cursor = db.getSiblingDB("teste").deliveryOrder.find({$and:[ {"_id" :{$gte: ObjectId.fromDate(new Date(d1))}}, {"_id":{$lte: ObjectId.fromDate(new Date(d2))}}]})
- var tot = cursor.count()
+	
+ print("Comecei as: "+Date())
+	var cursor = db.getSiblingDB("teste").events.find({$and:[ {"_id" :{$gte: ObjectId.fromDate(new Date(d1))}}, {"_id":{$lte: ObjectId.fromDate(new Date(d2))}}]})
+	var tot = cursor.count()
 
-cursor.forEach(function(d){db.getSiblingDB("teste").deliveryOrder.remove({_id: d._id})+print("Removidos os ids "+d._id)})
-print("Total: "+tot)
-
+	cursor.forEach(function(d){db.getSiblingDB("teste").events.remove({_id: d._id})})
+	print("Total: "+tot)
+	print("Terminei as: "+Date())
 }
